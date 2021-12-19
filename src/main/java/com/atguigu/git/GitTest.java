dbf90919cf1e54f2dec2b4bb23c4ece66cec6494
zhoufengping <zhoufengping99@163.com>
2021-12-19 20:16:58 +0800

    hot-fix commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!");
+        System.out.println("hot-fix test!");
     }
 
 }
